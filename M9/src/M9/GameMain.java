bce241d74dd0e6ee2f87e912a543cc813b8c0407
pontusgustafsson <pontusgustafsson@192.168.2.245>
2020-03-16 23:21:36 +0100

    Namnet på spelet

--- M9/src/M9/GameMain.java
+++ M9/src/M9/GameMain.java
@@ -3,7 +3,7 @@ package M9;
 public class GameMain {
 
 	   public static void main(String[] args) {
-	       GameView gv = new GameView(800, 600,"Game");
+	       GameView gv = new GameView(800, 600,"Game Moment 9");
 	       GameController gc = new GameController(gv);
 	    
 	       gc.run();
