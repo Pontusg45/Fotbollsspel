7738eff3871522bd0ca036a2a01bf0990d0f603f
pontusgustafsson <pontusgustafsson@10.20.24.248>
2020-03-16 14:28:07 +0100

    bytt ut entitylist till spritleist lagt till vissa metoder

--- M9/src/M9/GameController.java
+++ M9/src/M9/GameController.java
@@ -4,21 +4,25 @@ import java.awt.Image;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 import javax.swing.ImageIcon;
 
-import sun.misc.GC;
 
 public class GameController implements KeyListener {
    private ShipEntity ship;
    private GameView gv;
    private boolean gameRunning = true;
    
+   Random rand = new Random();
+   
    Image shipImg = new ImageIcon(getClass().getResource("/ship.png")).getImage();
+   
+   Image alienImg = new ImageIcon(getClass().getResource("/alien.png")).getImage();
    
    private HashMap<String, Boolean> keyDown = new HashMap<>(); 
 
-   private ArrayList<Entity > entityList = new ArrayList<>();
+   Entity[] spriteList = new Entity[6];
 
    public GameController(GameView gv) {
        this.gv = gv;
@@ -36,32 +40,65 @@ public class GameController implements KeyListener {
    private void loadImages() {
        
        ship = new ShipEntity(shipImg, 300, 300, 30);
-       entityList.add(ship);
-       entityList.add(new AlienEntity(new ImageIcon(getClass().getResource("/alien.png")).getImage(), 100,40,20));
-       entityList.add(new AlienEntity(new ImageIcon(getClass().getResource("/alien.png")).getImage(), 200,40,20));
-       entityList.add(new AlienEntity(new ImageIcon(getClass().getResource("/alien.png")).getImage(), 300,40,20));
-       entityList.add(new AlienEntity(new ImageIcon(getClass().getResource("/alien.png")).getImage(), 400,40,20));
+       
+       spriteList[0] = ship;
+       spriteList[1] = new AlienEntity(alienImg, rand.nextInt(gv.getWidth() / alienImg.getWidth(null))* alienImg.getWidth(null),-100,20);
+       spriteList[2] = new AlienEntity(alienImg, rand.nextInt(gv.getWidth() / alienImg.getWidth(null))* alienImg.getWidth(null),-300,20);
+       spriteList[3] = new AlienEntity(alienImg, rand.nextInt(gv.getWidth() / alienImg.getWidth(null))* alienImg.getWidth(null),-500,20);
+       spriteList[4] = new AlienEntity(alienImg, rand.nextInt(gv.getWidth() / alienImg.getWidth(null))* alienImg.getWidth(null),-700,20);
+       spriteList[5] = new AlienEntity(alienImg, rand.nextInt(gv.getWidth() / alienImg.getWidth(null))* alienImg.getWidth(null),-900,20);
    }
 
    private void update(long deltaTime) {
 	   
 	   if(keyDown.get("right") && ship.getxPos() < gv.getWidth() - shipImg.getWidth(null) ) { 
-       	ship.setDirectionX(1); 
-   	}else if(keyDown.get("left") && ship.getxPos()> 0) { 
-       	ship.setDirectionX(-1); 
-   	}else {
-       	ship.setDirectionX(0);
-   	}
-	   if(keyDown.get("space")) {
-  		 ship.tryToFire();
-  	}   
-       for(Entity e : entityList) {
-           e.move(deltaTime);
-       }
+       		ship.setDirectionX(1); 
+   		}else if(keyDown.get("left") && ship.getxPos()> 0) { 
+   			ship.setDirectionX(-1); 
+   		}else {
+   			ship.setDirectionX(0);
+   		}
+	   	if(keyDown.get("space")) {
+	   		ship.tryToFire();
+	   	}   
+     
+	   	alienMove(deltaTime);
+	   	
+	   	ship.move(deltaTime);
+	   	
+	   	checkCollisionAndRemove();
    }
+   public void alienMove(long deltaTime) {
+   	
+		for (int i = 1 ; i< spriteList.length ; i++) {
+			if (spriteList[i].getyPos() < gv.getHeight()) {
+				spriteList[i].setDirectionX(1);
+			    spriteList[i].move(deltaTime);
+			}
+			else { 
+				spriteList[i].setyPos(-1 * alienImg.getHeight(null));
+				spriteList[i].setxPos(rand.nextInt(gv.getWidth() / alienImg.getWidth(null)) * alienImg.getWidth(null));
+			}
+		}
+	}
+   
+   public void checkCollisionAndRemove(){
+	    ArrayList<Entity> removeList = new ArrayList<>();
+
+	    // alien <-> missile
+	    if(ship.missile != null && ship.missile.getActive()){
+	    	for(int i= 1; i < spriteList.length ; i++) {
+	    		if(ship.missile.collision(spriteList[i])) {
+	    			ship.missile.setActive(false);
+	    			removeList.add(spriteList[i]);	
+	    		}	
+	    	}
+	    }
+	    //spriteList.removeAll(removeList);// Alt namnet på arraylist  
+	}
    
    public void render() {
-       gv.render(entityList);
+       gv.render(spriteList);
    }
 
    public void run() {
