458632ebbe8e70413df58b200bdd325d93b4962d
pontusgustafsson <pontusgustafsson@192.168.2.245>
2020-03-17 11:49:22 +0100

    Rensat

--- M9/src/M9/TextEntity.java
+++ M9/src/M9/TextEntity.java
@@ -6,20 +6,21 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 
 public class TextEntity extends Entity {
+	
 	private int xPos, yPos;
-	private String txt = "";
+	private String text;
 	private Font font = null;
 	private Color color = null;
-	
 
-	public TextEntity(String txt, int xPos, int yPos, Font font, Color color) {
+	public TextEntity(String text, int xPos, int yPos, Font font, Color color) {
 		super(xPos, yPos);
-		this.txt = txt;
+		this.text = text;
 		this.xPos = xPos;
 		this.yPos = yPos;
 		this.font = font;
 		this.color = color;
 	}
+	
 	@Override
 	public void draw(Graphics2D g) {	
 		if(font != null) {
@@ -27,73 +28,39 @@ public class TextEntity extends Entity {
 			g.setFont(font);
 		}	
 		g.setColor(color);
-		g.drawString(txt, xPos, yPos);
+		g.drawString(text, xPos, yPos);
 	}
-	
 	
 	public void setXPos(int xPos) {
 		this.xPos = xPos;
 	}
-	
-	/**
-	 * Ãndrar y-pos 
-	 * @param y-pos 
-	 */
-	public void setYPos(int yPos) {
-		this.yPos = yPos;
-	}
-	/**
-	 * x-koordinaten
-	 * @return x-koordinaten 
-	 */
 	public int getXPos() {
 		return xPos;
 	}
 	
-	/**
-	 * x-koordinaten
-	 * @return x-koordinaten
-	 */
+	public void setYPos(int yPos) {
+		this.yPos = yPos;
+	}
 	public int getYPos() {
 		return yPos;
 	}
 	
-	/**
-	 * Ãndrar texten
-	 * @param txt 
-	 */
-	public void setTxt(String txt) {
-		this.txt = txt;
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getText() {
+		return text;
 	}
 	
-	/**
-	 * Texten i containern
-	 * @return texten
-	 */
-	public String getTxt() {
-		return txt;
-	}
-	
-	/**
-	 * Ãndrar typsnitt
-	 * @param font fonten
-	 */
 	public void setFont(Font font) {
 		this.font = font;
 	}
-	
-	/**
-	 * Ãndrar textfärgen
-	 * @param color FÃ¤rgen 
-	 * @see java.awt.Color
-	 */
 	public void setFont(Color color) {
 		this.color = color;
 	}
 	@Override
 	public void move(long deltaTime) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
 	
 }
