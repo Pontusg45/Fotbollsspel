84b2d480274dfbe543f68f7f121ac2dc95c19956
pontusgustafsson <pontusgustafsson@192.168.2.245>
2020-03-24 10:28:46 +0100

    Butade namn på spelet

--- M9/src/M9/GameMain.java
+++ M9/src/M9/GameMain.java
@@ -3,7 +3,7 @@ package M9;
 public class GameMain {
 
 	   public static void main(String[] args) {
-	       GameView gv = new GameView(800, 600,"Game Moment 9");
+	       GameView gv = new GameView(800, 600,"Enkelt Fotbollsspel");
 	       GameController gc = new GameController(gv);
 	    
 	       gc.run();
