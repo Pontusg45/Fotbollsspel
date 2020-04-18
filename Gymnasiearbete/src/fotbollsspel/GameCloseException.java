e94bf685573cca9ce53c5e4ca36d8d55236bdcf4
pontusgustafsson <pontusgustafsson@192.168.2.245>
2020-04-18 18:04:12 +0200

    Raderade katalog

--- Gymnasiearbete/src/fotbollsspel/GameCloseException.java
+++
@@ -1,9 +0,0 @@
-package fotbollsspel;
-
-public class GameCloseException extends Exception{
-	private static final long serialVersionUID = -1822574920170126156L;
-
-	public GameCloseException(String message) {
-		super(message);
-	}
-}
